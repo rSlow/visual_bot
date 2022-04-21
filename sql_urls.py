5138d6d6293fdc1343aa9efc1d83f54fb1483d29
redSlow <redmaxx4@gmail.com>
2022-04-21 13:00:07 +1000

    To aiosqlite

--- sql_urls.py
+++ sql_urls.py
@@ -1,4 +1,1 @@
-import os
-
-DATABASE_URL_ENV = os.getenv("DATABASE_URL")
-DATABASE_URL = "postgresql+asyncpg" + DATABASE_URL_ENV[DATABASE_URL_ENV.find(":"):]
+DATABASE_URL = "sqlite+aiosqlite:///database.db"
--- start.py
+++ start.py
@@ -2,19 +2,19 @@ from aiogram import executor
 
 from bot import dp
 from startup import on_startup, on_shutdown
-from webhook_settings import WEBAPP_HOST, WEBAPP_PORT, WEBHOOK_PATH
+# from webhook_settings import WEBAPP_HOST, WEBAPP_PORT, WEBHOOK_PATH
 
 if __name__ == '__main__':
-    executor.start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT
-    )
-    # executor.start_polling(dispatcher=dp,
-    #                        skip_updates=True,
-    #                        on_startup=on_startup,
-    #                        on_shutdown=on_shutdown)
+    # executor.start_webhook(
+    #     dispatcher=dp,
+    #     webhook_path=WEBHOOK_PATH,
+    #     skip_updates=True,
+    #     on_startup=on_startup,
+    #     on_shutdown=on_shutdown,
+    #     host=WEBAPP_HOST,
+    #     port=WEBAPP_PORT
+    # )
+    executor.start_polling(dispatcher=dp,
+                           skip_updates=True,
+                           on_startup=on_startup,
+                           on_shutdown=on_shutdown)
